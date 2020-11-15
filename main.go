661f879b37cf1766a36d529ba825586304058897
Maxwell Trussell <maxwell.trussell@salesforce.com>
2020-11-14 18:21:15 -0700

    dont re-notify products

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -44,12 +47,15 @@ func main() {
 	flag.Parse()
 
 	product_mapping := map[string]string{
-		"Rogue Olympic Plates":         "https://www.roguefitness.com/rogue-olympic-plates",
-		"Rogue Deep Dish Plates":       "https://www.roguefitness.com/rogue-deep-dish-plates",
-		"Rogue Steel Plates":           "https://www.roguefitness.com/rogue-calibrated-lb-steel-plates",
-		"Rogue Bumper Plates":          "https://www.roguefitness.com/rogue-hg-2-0-bumper-plates",
-		"Rep Fitness Cast Iron Plates": "https://www.repfitness.com/bars-plates/olympic-plates/iron-plates/rep-iron-plates",
-		"Rep Fitness Bumper Plates":    "https://www.repfitness.com/bars-plates/olympic-plates/bumper-plates/rep-black-bumper-plates",
+		"Rogue Olympic Plates":          "https://www.roguefitness.com/rogue-olympic-plates",
+		"Rogue Deep Dish Plates":        "https://www.roguefitness.com/rogue-deep-dish-plates",
+		"Rogue Steel Plates":            "https://www.roguefitness.com/rogue-calibrated-lb-steel-plates",
+		"Rogue Bumper Plates":           "https://www.roguefitness.com/rogue-hg-2-0-bumper-plates",
+		"Rogue Fleck Plate":             "https://www.roguefitness.com/rogue-fleck-plates",
+		"Rogue Echo Bumper Plate v2":    "https://www.roguefitness.com/rogue-echo-bumper-plates-with-white-text",
+		"Rogue Color Echo Bumper Plate": "https://www.roguefitness.com/rogue-color-echo-bumper-plate",
+		"Rep Fitness Cast Iron Plates":  "https://www.repfitness.com/bars-plates/olympic-plates/iron-plates/rep-iron-plates",
+		"Rep Fitness Bumper Plates":     "https://www.repfitness.com/bars-plates/olympic-plates/bumper-plates/rep-black-bumper-plates",
 	}
 
 	ch := make(chan []product)
@@ -65,29 +71,63 @@ func main() {
 
 	fmt.Println("")
 	fmt.Println("Available Products:")
+	already_notified := get_notified_products()
 	var notify_products []product
+	var watched_available_products []product
 	for _, p := range products {
 		if p.is_available() {
 			fmt.Println(p)
 			if watched(p) {
-				notify_products = append(notify_products, p)
+				watched_available_products = append(watched_available_products, p)
+				if !already_notified[p.name] {
+					notify_products = append(notify_products, p)
+				}
 			}
 		}
 	}
 
+	// Send telegram notification
 	if *telegram_api_ptr != "" && *telegram_chat_id_ptr != "" {
-		msg := "In Stock Items:\n"
+		msg := "Watched In Stock Items:\n"
 		for _, p := range notify_products {
 			msg += fmt.Sprintf("- %s\n", p.name)
 		}
 		if len(notify_products) > 0 {
+			fmt.Println()
+			fmt.Println("Sending notification...")
+			fmt.Println(msg)
 			send_telegram_message(
 				*telegram_api_ptr,
 				*telegram_chat_id_ptr,
 				msg,
 			)
 		}
+		// Store notified products, so as to not re-notify
+		var product_names []string
+		for _, p := range watched_available_products {
+			product_names = append(product_names, p.name)
+		}
+		err := ioutil.WriteFile("notified_products.txt", []byte(strings.Join(product_names, "\n")), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
+}
+
+func get_notified_products() map[string]bool {
+	file, err := os.Open("notified_products.txt")
+	if err != nil {
+		// The file probably does not exist
+		return map[string]bool{}
+	}
+	defer file.Close()
+
+	notified_products := make(map[string]bool)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		notified_products[strings.Trim(scanner.Text(), "\n")] = true
+	}
+	return notified_products
 }
 
 func send_telegram_message(api_token, chat_id, msg string) {
